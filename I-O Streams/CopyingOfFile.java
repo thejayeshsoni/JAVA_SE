6390353fa864bf8fac1e54085a01f56ec9c734ed
JAYESH SONI <67512410+thejayeshsoni@users.noreply.github.com>
2021-04-17 13:45:38 +0530

    Update CopyingOfFile.java

--- I-O Streams/CopyingOfFile.java
+++ I-O Streams/CopyingOfFile.java
@@ -4,10 +4,11 @@ import java.io.FileOutputStream;
 public class CopyingOfFile {
     public static void main(String[] args) {
         try {
+            
             FileInputStream fis = new FileInputStream(
-                    "C:/Users/Jayesh/Documents/JAVA PROGRAMS/I-O Streams/Source1.txt");
+                    "C:/Users/Jayesh/Documents/JAVA PROGRAMS/I-O Streams/Source1.txt");  // Location of File
             FileOutputStream fos = new FileOutputStream(
-                    "C:/Users/Jayesh/Documents/JAVA PROGRAMS/I-O Streams/Source2.txt");
+                    "C:/Users/Jayesh/Documents/JAVA PROGRAMS/I-O Streams/Source2.txt"); // Location of File or if there is no such file then this file will created by compiler.
 
             int b;
             while ((b = fis.read()) != -1) {
